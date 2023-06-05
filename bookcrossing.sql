a1a6903a56f592565639701d76ad4ac5caf2d0dd
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2023-06-05 22:27:39 +0200

    sql db sife

--- bookcrossing.sql
+++ bookcrossing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2023 at 10:31 AM
+-- Generation Time: Jun 05, 2023 at 10:26 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -37,10 +37,32 @@ CREATE TABLE `passwords` (
 --
 
 INSERT INTO `passwords` (`user_id`, `password`) VALUES
-(1, 'admin'),
-(2, 'q'),
-(3, 'qwer'),
-(5, 'ktos');
+(7, '8ccd4fc6b1974514102a698560d7a092'),
+(8, '8ccd4fc6b1974514102a698560d7a092');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `photo` varchar(256) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `author_book` varchar(256) NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `photo`, `title`, `author_book`, `user_id`) VALUES
+(1, '2.jpg', 'Jak ocalic swiat od katastrofy klimatycznej', 'Bill Gates', 8),
+(3, '815036-352x5001685994324.jpeg', 'Nabici w Facebooka', 'Roger McNamee', 7),
+(4, '936172-352x5001685996554.jpeg', '21 lekcji na XXI wiek', 'Yuval Noah Harari', 7),
+(5, '931929-352x5001685996615.jpeg', 'Antisocial Media', 'Siva Vaidhyanathan', 7);
 
 -- --------------------------------------------------------
 
@@ -61,11 +83,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `username`, `email`) VALUES
-(1, 'admin-name', '', 'admin', 'admin@gmail.com'),
-(2, 'Oleksandr', 'Bezverkhnii', '', 'dewalat485@gmail.com'),
-(3, 'qwer', 'qwer', 'qwer', 'qwer'),
-(4, 'cos', 'cos', 'cos', 'cos@gmail.com'),
-(5, 'ktos', 'ktos', 'ktos', 'ktos@gmail.com');
+(7, 'admin', 'admin', 'admin', 'admin@admin.com'),
+(8, 'Oleksandr', 'Bezverkhnii', 'bezverkhnii', 'email@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -75,6 +94,13 @@ INSERT INTO `users` (`id`, `name`, `surname`, `username`, `email`) VALUES
 -- Indexes for table `passwords`
 --
 ALTER TABLE `passwords`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -88,10 +114,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -102,6 +134,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `passwords`
   ADD CONSTRAINT `passwords_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
