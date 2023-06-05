a31089032caf431362a2439aecd78f29fca66953
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-06-05 22:42:46 +0200

    responsive header, fixed fixedBtn error

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,8 @@
 let userName = document.querySelector(".user-name")
 let menu = document.querySelector(".menu-wrapper")
 
+const fixedBtn = document.querySelector('.fixed-button')
+
 document.addEventListener("click", (e) => {
     if ([e.target, e.target.parentElement].includes(userName))
         menu.classList.toggle("hidden")
@@ -8,12 +10,11 @@ document.addEventListener("click", (e) => {
         menu.classList.add("hidden")
     
 })
-
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset >= 120) {
-        document.querySelector('.fixed-button').classList.add('show')
-    }
-    else
-        document.querySelector('.fixed-button').classList.remove('show')
-
-  });
+if (fixedBtn)
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset >= 120) {
+            fixedBtn.classList.add('show')
+        }
+        else
+            fixedBtn.classList.remove('show')
+    });
