e1f4b392532570d498ed49b605848dbd9a6c2077
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2023-06-05 19:52:49 +0200

    charged upload img button

--- pages/upload_posts.php
+++ pages/upload_posts.php
@@ -1,9 +1,16 @@
-<div class="overlay hidden">
+<div class="overlay">
     <div class="content">
-      <p id="public">Opublikuj książkę</p>
-      <div class="input-container">
-        <input type="text" name="title" placeholder="Tytuł" id="title">
-        <input type="text" name="author" placeholder="Autor książki" id="author">
-      </div>
+        <p id="public">Opublikuj książkę</p>
+        <form>
+            <div class="input-container">
+                <input type="text" name="title" placeholder="Tytuł" id="title">
+                <input type="text" name="author" placeholder="Autor książki" id="author">
+                <div class="upload-container">
+                    <label class="upload-label">Załącz zdjęcie książki:</label>
+                    <input type="file" accept="image/*" class="upload">
+                </div>
+            </div>
+            <!-- <button class="submit-button">Dalej</button> -->
+        </form>
     </div>
   </div>
